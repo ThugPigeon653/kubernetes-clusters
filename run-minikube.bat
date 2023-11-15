3352cea4b3f34c330b9917827ebbc2d5b6179d9a
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-15 15:55:08 +1000

    minikube cluster working

--- run-minikube.bat
+++ run-minikube.bat
@@ -2,23 +2,23 @@ REM The ps1 script installs minikube and sets up PATH. The rest of this script j
 REM Replace the application in web-app/, and that app will be launched on the cluster
 powershell -ExecutionPolicy Bypass -File "%CD%\minikube-setup.ps1"
 cd web-app
-docker build -t web-app/py-app .
-cd ..
-kubectl get deployment hello-minikube > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Creating deployment...
-    kubectl create deployment hello-minikube --image=web-app/py-app:latest
-) else (
-    echo Deployment already exists.
-)
-kubectl get service hello-minikube > nul 2>&1
-if %errorlevel% neq 0 (
-    echo Creating service...
-    kubectl expose deployment hello-minikube --type=NodePort --port=8080
-) else (
-    echo Service already exists.
-)
-kubectl get services hello-minikube
+docker login
+docker build -t thugpigeon653/lneilsen-demo:latest .
+docker push thugpigeon653/lneilsen-demo:latest
+minikube stop
+minikube config set driver docker
+minikube delete
+minikube start --memory=4096 --cpus=2
+minikube kubectl -- get po -A
+call minikube -p minikube docker-env
+echo Deleting existing deployment...
+minikube kubectl -- delete deployment hello-minikube
+echo Creating deployment...
+minikube kubectl -- create deployment hello-minikube --image=thugpigeon653/lneilsen-demo:latest
+minikube kubectl -- expose deployment hello-minikube --type=NodePort --port=5000
+minikube kubectl -- get service hello-minikube
 minikube service hello-minikube
 minikube dashboard
+echo PRESS ANY KEY TO STOP MINIKUBE
 pause
+minikube stop
