5915a3919eddd74e96af4d5d4dce4034933775fd
ThugPigeon653 <89567974+ThugPigeon653@users.noreply.github.com>
2023-11-15 11:39:29 +1000

    now runs app in web-app folder

--- run-minikube.bat
+++ run-minikube.bat
@@ -1,6 +1,7 @@
 REM The ps1 script installs minikube and sets up PATH. The rest of this script just launches a cluster and starts the dashboard. Replace the image with any docker image
 powershell -ExecutionPolicy Bypass -File "%CD%\minikube-setup.ps1"
-kubectl create deployment hello-minikube --image=kicbase/echo-server:1.0
+docker build -t web-app/py-app build/.
+kubectl create deployment hello-minikube --image=web-app/py-app:latest
 kubectl expose deployment hello-minikube --type=NodePort --port=8080
 kubectl get services hello-minikube
 minikube service hello-minikube
